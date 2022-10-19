5dbefb320a5ace4e25d33a6c63eb6bbd9ed0cdc1
f-sky <chenlinghao@zju.edu.cn>
2022-10-19 19:43:53 +0800

    add nms

--- disprcnn/csrc/vision.cpp
+++ disprcnn/csrc/vision.cpp
@@ -4,7 +4,7 @@
 #include "ROIPool.h"
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-//  m.def("nms", &nms, "non-maximum suppression");
+  m.def("nms", &nms, "non-maximum suppression");
   m.def("roi_align_forward", &ROIAlign_forward, "ROIAlign_forward");
   m.def("roi_align_backward", &ROIAlign_backward, "ROIAlign_backward");
   m.def("roi_pool_forward", &ROIPool_forward, "ROIPool_forward");
